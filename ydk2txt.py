4051a67de2f2bfb5a10c9bc4988f18fa7573a7e2
Metalgearay <palbook@hotmail>
2016-08-18 23:05:36 -0400

    Added ability to search for prices using yugioh prices and also refactored the code to be more clean

--- ydk2txt.py
+++ ydk2txt.py
@@ -1,57 +1,105 @@
 from collections import Counter
+import urllib, json
 import sqlite3
 import sys
-deckname=sys.argv[1]
-outputfile=sys.argv[2]
-alist = [line.rstrip() for line in open(deckname)]
-maindec=alist[alist.index("#main")+1:alist.index("#extra")]
-extradec=alist[alist.index("#extra")+1:alist.index("!side")]
-sidedec=alist[alist.index("!side")+1:]
-maindecz=Counter(maindec)
-extradecz=Counter(extradec)
-sidedecz=Counter(sidedec)
 
-f=open(outputfile,"w")
-f.write("Main Deck:\n")
-f.write("\n")
-conn = sqlite3.connect('cards.cdb')
-cur = conn.cursor()
-conn2 = sqlite3.connect('official.cdb')
-cur2 = conn2.cursor()
-for keys in maindecz:	
-	ids=(keys,)
-	cur.execute("SELECT name FROM texts WHERE id=?",ids)
-	temp=cur.fetchone()
-	if temp is not None:
-		f.write(str(temp[0])+" x "+str(maindecz[keys])+"\n")
+
+totalsum=0
+def get_price(name):
+	url="http://yugiohprices.com/api/get_card_prices/"
+	url=url+str(name)
+	res=urllib.urlopen(url)
+	content=json.load(res)
+	if content['status'] != "fail": 
+		print_tag=content['data'][0]['print_tag']
+		price=float(content['data'][0]['price_data']['data']['prices']['low'])
+		return (print_tag,price,)
 	else:
-		cur2.execute("SELECT name FROM texts WHERE id=?",ids)
-		result=cur2.fetchone()
-		f.write(str(result[0])+" x "+str(maindecz[keys])+"\n")
-f.write("\n")
-f.write("Extra Deck:\n")
-f.write("\n")
-for keys in extradecz:	
-	ids=(keys,)
-	cur.execute("SELECT name FROM texts WHERE id=?",ids)
-	temp=cur.fetchone()
-	if temp is not None:
-		f.write(str(temp[0])+" x "+str(extradecz[keys])+"\n")
+		print("Could not find "+name)
+		return None
+
+def openDatabase():
+	conn = sqlite3.connect('cards.cdb')
+	cur = conn.cursor()
+	return cur
+
+def createDeckLists(deckname):
+	alist = [line.rstrip() for line in open(deckname)]
+	maindec=alist[alist.index("#main")+1:alist.index("#extra")]
+	extradec=alist[alist.index("#extra")+1:alist.index("!side")]
+	sidedec=alist[alist.index("!side")+1:]
+	maindecz=Counter(maindec)
+	extradecz=Counter(extradec)
+	sidedecz=Counter(sidedec)
+	return (maindecz,extradecz,sidedecz)
+
+def searchForName(database,id):
+	database.execute("SELECT name FROM texts WHERE id=?",id)
+	result=database.fetchone()
+	if result is not None:
+		return result
 	else:
-		cur2.execute("SELECT name FROM texts WHERE id=?",ids)
-		result=cur2.fetchone()
-		f.write(str(result[0])+" x "+str(extradecz[keys])+"\n")
-f.write("\n")
-f.write("Side Deck:\n")
-f.write("\n")
-for keys in sidedecz:	
-	ids=(keys,)
-	cur.execute("SELECT name FROM texts WHERE id=?",ids)
-	temp=cur.fetchone()
-	if temp is not None:
-		f.write(str(temp[0])+" x "+str(sidedecz[keys])+"\n")
-	else:
-		cur2.execute("SELECT name FROM texts WHERE id=?",ids)
-		result=cur2.fetchone()
-		f.write(str(result[0])+" x "+str(sidedecz[keys])+"\n")
-f.close()
+		("Could not find %s please check if you have update data base" % id,)
+
+def writeToFile(database,string,deck,mainFile,pricerpt,pricefl):
+	mainFile.write("\n%s\n\n" % string)
+	sums=0
+	if pricerpt == "y":
+		try:
+			pricefl.write("%s:\n\n" % string)
+		except:
+
+			print ("Error reading file")
+	for key in deck:
+		search=(key,)
+		name=searchForName(database,search)
+		mainFile.write("%s x %s\n" % (name[0],deck[key]))
+		if pricerpt == "y":
+			tup=get_price(name[0])
+			if tup is not None:
+					print_tag=str(tup[0])
+					price=str(tup[1]*deck[key])
+					pricefl.write("%s - %s x %s $%s USD \n" % (name[0],print_tag,deck[key],price))
+					sums+=float(price)
+			else:
+				pricefl.write("%s - Could not find price\n"% name[0])
+	if pricerpt == "y":
+		try:
+			pricefl.write("\n%s Total : $%s USD \n\n" % (string,sums))
+			global totalsum
+			totalsum+=sums
+		except:
+			print ("Error reading file")
+
+def main():
+	deckname=sys.argv[1]
+	outputfile=sys.argv[2]
+	filetowrite=open(outputfile,'w')
+	pricefl=None
+	answer=raw_input("Would you like to generate a price report(y/n)\n?")
+	if answer == 'y':
+		filename=raw_input("Please enter name you would like for the price report\n")
+		pricefl=open("%s.txt" % filename,'w')
+	decks=createDeckLists(deckname)
+	maindeck=decks[0]
+	extradeck=decks[1]
+	sidedeck=decks[2]
+	database=openDatabase()
+	writeToFile(database,"Main Deck",maindeck,filetowrite,answer,pricefl)
+	writeToFile(database,"Extra Deck",extradeck,filetowrite,answer,pricefl)
+	writeToFile(database,"Side Deck",sidedeck,filetowrite,answer,pricefl)
+	filetowrite.close()
+	if answer == 'y':
+		try:
+			global totalsum
+			pricefl.write("Deck total: $%s USD" % totalsum)
+			pricefl.close()
+		except:
+			print("Error")
+			pricefl.close()
+			
+if __name__ == "__main__":
+	main()
+
+
+
